81b24702ef140791877ecfd1c062ffe48a5989fd
prantae <neelratan@live.com>
2023-05-24 21:01:22 +0530

    init

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+  
+  return (
+    <View style={styles.sectionContainer}>
+      <StatusBar />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1
+  }
+});
+
+export default App;
